623636af1ca60ede16d1ca45b028c65a524cec2a
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2022-04-10 23:01:07 +0430

    implement migrations

--- database/migrations/2022_04_10_132509_create_roles_table.php
+++ database/migrations/2022_04_10_132509_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
         });
     }
 
