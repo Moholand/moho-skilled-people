23e64725f9013e51673b0faf8992f518dfde9a1e
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2022-04-16 19:02:11 +0430

    add candidate controller

---
+++ app/Http/Controllers/Api/CandidateController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Candidate;
+use App\Http\Controllers\Controller;
+use App\Services\CandidateService;
+use App\Http\Resources\Candidate\CandidateResource;
+use App\Http\Requests\Candidate\CandidateUpdateRequest;
+
+class CandidateController extends Controller
+{
+    /**
+     * @var CandidateService $candidateService
+     */
+    protected $candidateService;
+
+    /**
+     * CandidateController constructor.
+     *
+     * @param  CandidateService $candidateService
+     */
+    public function __construct(CandidateService $candidateService)
+    {
+        $this->candidateService = $candidateService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        $this->authorize('viewAny', Candidate::class);
+
+        $candidates = $this->candidateService->allCandidates();
+
+        return CandidateResource::collection($candidates);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  CandidateUpdateRequest $request
+     * @param  Candidate $candidate
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(CandidateUpdateRequest $request, Candidate $candidate)
+    {
+        $this->authorize('update', $candidate);
+
+        $candidate = $this->candidateService->updateCandidate($request->validated(), $candidate->id);
+
+        return response()->json([
+            'candidate' => new CandidateResource($candidate),
+            'message' => 'Candidate updated successfully'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Candidate $candidate
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy(Candidate $candidate)
+    {
+        $this->authorize('delete', $candidate);
+
+        $this->candidateService->deleteCandidate($candidate->id);
+
+        return response()->json(['message' => 'Candidate deleted successfully']);
+    }
+}
