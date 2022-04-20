9c268ee45bbb23b90be86931c0d00ccc824761e5
Mohammad Akbarzadeh <moho.akbarzadeh@gmail.com>
2022-04-20 11:39:22 +0430

    create EmployerRepository

---
+++ app/Repositories/EmployerRepository.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Employer;
+use App\Models\User;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class EmployerRepository
+{
+    /**
+     * Get all the employers.
+     *
+     * @return LengthAwarePaginator
+     */
+    public function allEmployers(): LengthAwarePaginator
+    {
+        return Employer::with('users')->paginate(20)->withQueryString();
+    }
+
+    /**
+     * Store a new employer data.
+     *
+     * @param  array $data
+     * @param  User $user
+     * @return \App\Models\Employer|\Illuminate\Database\Eloquent\Model
+     */
+    public function storeEmployer(array $data, User $user): Employer
+    {
+        return $user->employers()->create($data);
+    }
+
+    /**
+     * Get the specified employer.
+     *
+     * @param  int  $id
+     * @return \App\Models\Employer|\Illuminate\Database\Eloquent\Model
+     */
+    public function getEmployer(int $id): Employer
+    {
+        return Employer::with('users')->findOrFail($id);
+    }
+
+    /**
+     * Get one employer data by id, whether trashed or not.
+     *
+     * @param  int $id
+     * @return \App\Models\Employer
+     */
+    public function getEmployerWithTrashed(int $id): Employer
+    {
+        return Employer::withTrashed()->findOrFail($id);
+    }
+
+    /**
+     * Update employer data.
+     *
+     * @param  array $data
+     * @param  int $id
+     * @return \App\Models\Employer
+     */
+    public function updateEmployer(array $data, int $id): Employer
+    {
+        $employer = $this->getEmployer($id);
+
+        $employer->update($data);
+
+        return $employer;
+    }
+
+    /**
+     * Delete employer from storage.
+     *
+     * @param  int $id
+     * @return String
+     */
+    public function deleteEmployer(int $id): string
+    {
+        $employer = $this->getEmployerWithTrashed($id);
+
+        if($employer->trashed()) {
+            $employer->forceDelete();
+            return 'Employer deleted successfully';
+        } else {
+            $employer->delete();
+            return 'Employer moved to trashed successfully';
+        }
+    }
+}
